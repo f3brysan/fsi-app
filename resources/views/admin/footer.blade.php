f2cfffadc4dcb8d8ba2f144904fa7c5de04a1fac
febrysan <febrysan1995@gmail.com>
2022-03-09 05:18:28 +0700

    Initial Commit 1.0

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                2022 © Federasi Supra Indonesia.
+            </div>            
+        </div>
+    </div>
+</footer>
+
+</div>
+<!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+
+<!-- JAVASCRIPT -->
+<script src="assets/libs/jquery/jquery.min.js"></script>
+<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="assets/libs/simplebar/simplebar.min.js"></script>
+<script src="assets/libs/node-waves/waves.min.js"></script>
+
+<script src="assets/js/app.js"></script>
+
+</body>
+</html>

263ca31df4a491bf4993487c47af8996e7c5263a
PEPPY\febry <febrysan@hotmail.com>
2022-05-10 05:02:00 +0700

    Pembuatan Fitur Regional

---
+++ database/migrations/2022_05_09_202103_create_regionals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regionals', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid',36);
+            $table->string('nama',50);
+            $table->dateTime('verified_at')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regionals');
+    }
+}

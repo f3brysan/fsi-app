6f2a3049d0e6d4ce806626d2016e083f90e0adf2
PEPPY\febry <febrysan@hotmail.com>
2022-05-18 06:10:08 +0700

    soft delete pada model komunitas

--- app/Models/Komunitas.php
+++ app/Models/Komunitas.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use App\Models\Regional;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Komunitas extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
 
     protected $fillable = [
         'uuid',

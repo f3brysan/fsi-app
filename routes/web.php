214eef714146d07c4ea36e5e8a107ecd518faf4b
f3brysan <febrysan1995@gmail.com>
2022-06-03 14:30:29 +0700

    Giving middleware into home route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\BiodataController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KomunitasController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PengpusEventController;
 use App\Http\Controllers\RegionalController;
 use App\Http\Controllers\RegisterController;
+use App\Models\Biodata;
 use GuzzleHttp\Middleware;
 
 /*
@@ -23,7 +26,7 @@ use GuzzleHttp\Middleware;
 Route::get('/reload-captcha', [RegisterController::class, 'reloadCaptcha']);
 
 // routing login + logout 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'auth']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
@@ -31,13 +34,16 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/home', [DashboardController::class, 'index']);
+Route::get('/home', [DashboardController::class, 'index'])->middleware('auth');
+Route::resource('/biodata', BiodataController::class)->middleware('auth');
 
 // ROUTING MASTER REGIONAL
-Route::resource('regional',RegionalController::class);
+Route::resource('pengpus/regional',RegionalController::class);
 // Route::get('/regional', [RegionalController::class, 'index'])->Middleware('auth');
 
-Route::resource('komunitas',KomunitasController::class);
+Route::resource('pengpus/komunitas',KomunitasController::class);
+
+Route::resource('pengpus/event',PengpusEventController::class);
 
 Route::get('/agenda', function () {
     return view('admin/agenda');

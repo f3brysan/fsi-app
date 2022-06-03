6ea1ec1f4d41ad6e7c3baf668f5e76e402d2281b
f3brysan <febrysan1995@gmail.com>
2022-06-03 14:27:09 +0700

    redirect user to bio's page when no data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Biodata;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('admin.dashboard');
+    public function index()
+    {
+        $session = Auth::user()->uuid;
+        $biodata = Biodata::where('user_uuid', $session)->first();
+
+        if ($biodata){
+            return view('admin.dashboard', [
+                'biodata' => $biodata
+            ]);
+        }
+        else {
+            return redirect()
+            ->route('biodata.create')
+            ->with(['error' => 'Harap isi biodata Anda terlebih dahulu']);
+        }   
+       
     }
 }

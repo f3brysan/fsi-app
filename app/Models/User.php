2a53da463f4332a1e3540ad59e4e715fb01e609b
f3brysan <febrysan1995@gmail.com>
2022-06-03 14:28:38 +0700

    ORM user to biodata

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Biodata()
+    {
+        return $this->hasOne(Biodata::class,'user_uuid','uuid');
+    }
 }
